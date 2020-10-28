aa16eec54542b15d7fe2f83bc0f663fa40eacaa8
aakashbansal837 <aakashbansal837@gmail.com>
2020-10-28 20:09:44 +0530

    + get all data

--- frontend/src/component/View.js
+++ frontend/src/component/View.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from "./header";
+import axios from "axios";
+import { serverURL } from "../constants";
 
-const View = () => {
-  return (
-    <div>
-      <Header />
-      this is View option
-    </div>
-  );
-};
+class View extends React.Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(serverURL + "/user/")
+      .then((response) => {
+        console.log("data :", response);
+        // this.setState({ data: response });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        this is View option
+        {this.state.data[0]}
+      </div>
+    );
+  }
+}
 
 export default View;
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,1 @@
+export const serverURL = "http://127.0.0.1:4000";
